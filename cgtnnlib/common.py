ff2906aab01f4a59173090044fb1439863719745
Maria Zaitseva <maria@tail-call.ru>
2024-11-23 19:15:36 +0700

    Настали библиотечные времена

---
+++ cgtnnlib/common.py
@@ -0,0 +1,685 @@
+## COMMON LIBRARY v.0.1
+## Created at Sat 23 Nov 2024
+
+## 1.4.-2 Imports
+
+import os
+import json
+import urllib.request
+
+from dataclasses import dataclass
+from typing import List, Tuple
+
+from IPython.display import clear_output
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+import matplotlib.pyplot as plt
+
+## 1.4.-1,5 Enable CUDA
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+## 1.4.-1 Configuration
+
+REPORT_DIR = "report/"
+
+TEST_SAMPLE_SIZE = 0.2
+ITERATIONS = 10
+RANDOM_STATE = 23432
+BATCH_SIZE = 12
+EPOCHS = 20
+LEARNING_RATE = 0.00011
+PP = [0, 0.01, 0.05, 0.5, 0.9, 0.95, 0.99]
+PRINT_TRAINING_SPAN = 500
+
+NOISE_SAMPLES_COUNT = 50
+NOISE_FACTORS = [
+    x * 2/NOISE_SAMPLES_COUNT for x in range(NOISE_SAMPLES_COUNT)
+]
+
+## 1.4.0 Training related dataclasses
+
+@dataclass
+class ExperimentParameters:
+    iteration: int
+    p: float
+
+@dataclass
+class DatasetData:
+    train_dataset: pd.DataFrame
+    test_dataset: pd.DataFrame
+    train_loader: DataLoader
+    test_loader: DataLoader
+
+@dataclass
+class Dataset:
+    name: str
+    number: int
+    features_count: int
+    classes_count: int
+    data: DatasetData
+
+    def model_a_path(self, params: ExperimentParameters) -> str:
+        # XXX PthPath config variable?
+        return f'pth/model-{self.number}A-c-P{params.p}_N{params.iteration}.pth'
+
+    def model_b_path(self, params: ExperimentParameters) -> str:
+        return f'pth/model-{self.number}B-c-P{params.p}_N{params.iteration}.pth'
+
+## 1.4.1 Training library
+
+@dataclass
+class TrainingParameters:
+    dataset: Dataset
+    criterion: nn.CrossEntropyLoss | nn.MSELoss
+    experiment_params: ExperimentParameters
+    model_a_path: str
+    model_b_path: str
+    loss_curve_plot_col_index: int
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
+
+def print_progress(
+    p: float,
+    iteration: int,
+    epoch: int,
+    total_epochs: int,
+    total_samples: int,
+    running_loss: float,
+    dataset_number: int
+):
+    clear_output(wait=True)
+    print(
+        f'N={iteration} #{dataset_number} p={p} E{epoch}/{total_epochs} S{total_samples} Loss={running_loss / 100:.4f}'
+    )
+
+def train(
+    model, 
+    dataset: Dataset,
+    epochs: int,
+    experiment_parameters: ExperimentParameters,
+    criterion,
+    optimizer
+) -> list[float]:
+    running_losses: list[float] = []
+
+    for epoch in range(epochs):
+        running_loss = 0.0
+
+        model.train()
+
+        for i, (inputs, labels) in enumerate(dataset.data.train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+
+            outputs =  outputs.to(device)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if i % PRINT_TRAINING_SPAN == 0:
+                print_progress(
+                    p=experiment_parameters.p,
+                    iteration=experiment_parameters.iteration,
+                    epoch=epoch + 1,
+                    total_epochs=epochs,
+                    total_samples=len(dataset.data.train_loader),
+                    running_loss=running_loss,
+                    dataset_number=dataset.number
+                )
+
+            running_losses.append(running_loss)
+            running_loss = 0.0
+    
+    return running_losses
+
+def save_plot(filename_without_extension: str) -> str:
+    path = os.path.join(REPORT_DIR, f'{filename_without_extension}.png')
+    # plt.savefig(path)
+    plt.close()
+    return path
+
+def plot_loss_curve(
+    ax, # Axes
+    model_name: str,
+    dataset_name: str,
+    dataset_number: int,
+    running_losses: list[float],
+    p: float,
+    iteration: int,
+):
+    X = range(len(running_losses))
+    
+    ax.plot(X, running_losses, label='Running loss', color='orange')
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Value')
+    ax.set_title(f'Loss curve: {model_name} on {dataset_name} (#{dataset_number}), p = {p}, N = {iteration}')
+    ax.legend()
+
+def iterate_experiment_parameters():
+    for iteration in range(0, ITERATIONS):
+        for p in PP:
+            yield ExperimentParameters(iteration, p)
+
+## 1.4.2 Report generation
+
+if not os.path.exists(REPORT_DIR):
+    os.makedirs(REPORT_DIR)
+
+report_data = {}
+
+def append_to_report(label, data):
+    report_data[label] = data
+
+def save_report():
+    path = os.path.join(REPORT_DIR, 'report.json')
+    with open(path, 'w') as file:
+        json.dump(report_data, file, indent=4)
+    print(f"Отчёт сохранён в {path}")
+    
+## 1.4.3 Learning task types
+
+@dataclass
+class LearningTask:
+    criterion: object
+    dtype: torch.dtype
+
+CLASSIFICATION_TASK = LearningTask(
+    criterion=nn.CrossEntropyLoss(),
+    dtype=torch.long
+)
+
+REGRESSION_TASK = LearningTask(
+    criterion=nn.MSELoss(),
+    dtype=torch.float
+)
+
+## 1.4.4 Dataset utilities
+
+def download_csv(
+    url: str,
+    saved_name: str,
+    features: List[str] = None
+) -> pd.DataFrame:
+    data_dir = 'data'
+    os.makedirs(data_dir, exist_ok=True)
+    file_path = os.path.join(data_dir, saved_name)
+
+    urllib.request.urlretrieve(url, file_path)
+
+    if features is None:
+        return pd.read_csv(file_path)
+    else:
+        return pd.read_csv(file_path, header=None, names=features)
+
+def tensor_dataset_from_dataframe(
+    df: pd.DataFrame,
+    target: str,
+    y_dtype: type
+) -> TensorDataset:
+    X = df.drop(columns=[target]).values
+    y = df[target].values
+
+    X_tensor = torch.tensor(X, dtype=torch.float32)
+    y_tensor = torch.tensor(y, dtype=y_dtype)
+
+    return TensorDataset(X_tensor, y_tensor)
+
+## 1.4.5 Dataset #1
+
+def breast_cancer_dataset() -> Tuple[TensorDataset, TensorDataset]:
+    df = download_csv(
+        url='https://raw.githubusercontent.com/dataspelunking/MLwR/refs/heads/master/Machine%20Learning%20with%20R%20(2nd%20Ed.)/Chapter%2003/wisc_bc_data.csv',
+        saved_name='wisc_bc_data.csv'
+    )
+
+    target = 'diagnosis'
+
+    df[target] = df[target].map({ 'M': 0, 'B': 1 })
+    df = df.drop(columns=['id'])
+
+    train_df, val_df = train_test_split(
+        df,
+        test_size=TEST_SAMPLE_SIZE,
+        random_state=RANDOM_STATE
+    )
+
+    return (
+        tensor_dataset_from_dataframe(
+            df=train_df,
+            target=target,
+            y_dtype=CLASSIFICATION_TASK.dtype
+        ),
+        tensor_dataset_from_dataframe(
+            df=val_df,
+            target=target,
+            y_dtype=CLASSIFICATION_TASK.dtype
+        )
+    )
+
+## 1.4.6 Dataset #2
+
+def car_evaluation_dataset() -> Tuple[TensorDataset, TensorDataset]:
+    df = download_csv(
+        url='https://raw.githubusercontent.com/mragpavank/car-evaluation-dataset/refs/heads/master/car_evaluation.csv',
+        saved_name='car_evaluation.csv',
+        features=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+    )
+
+    target = 'class'
+
+    df[target] = df[target].map({
+        'unacc': 0,
+        'acc': 1,
+        'good': 2,
+        'vgood': 3,
+    })
+
+    df['doors'] = df['doors'].map({
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5more': 5
+    })
+
+    high_map = {
+        'low': 0,
+        'med': 1,
+        'high': 2,
+        'vhigh': 3
+    }
+
+    df['buying'] = df['buying'].map(high_map)
+    df['safety'] = df['safety'].map(high_map)
+    df['maint'] = df['maint'].map(high_map)
+
+    df['persons'] = df['persons'].map({
+        '2': 2,
+        '4': 4,
+        'more': 6
+    })
+
+    df['lug_boot'] = df['lug_boot'].map({
+        'small': 0,
+        'med': 1,
+        'big': 2
+    })
+
+    train_df, val_df = train_test_split(
+        df,
+        test_size=TEST_SAMPLE_SIZE,
+        random_state=RANDOM_STATE
+    )
+    return (
+        tensor_dataset_from_dataframe(
+            df=train_df,
+            target=target,
+            y_dtype=CLASSIFICATION_TASK.dtype
+        ),
+        tensor_dataset_from_dataframe(
+            df=val_df,
+            target=target,
+            y_dtype=CLASSIFICATION_TASK.dtype
+        )
+    )
+
+## 1.4.7 Dataset #3
+
+def student_performance_factors_dataset() -> Tuple[TensorDataset, TensorDataset]:
+    df = pd.read_csv('data/StudentPerformanceFactors.csv')
+
+    target = 'Exam_Score'
+
+    lmh = {
+        'Low': -1,
+        'Medium': 0,
+        'High': +1,
+    }
+
+    yn = {
+        'Yes': +1,
+        'No': -1,
+    }
+
+    df = df.dropna(subset=['Teacher_Quality'])
+
+    df['Parental_Involvement'] = df['Parental_Involvement'].map(lmh)
+    df['Access_to_Resources'] = df['Access_to_Resources'].map(lmh)
+    df['Extracurricular_Activities'] = df['Extracurricular_Activities'].map(yn)
+    df['Motivation_Level'] = df['Motivation_Level'].map(lmh)
+    df['Internet_Access'] = df['Internet_Access'].map(yn)
+    df['Family_Income'] = df['Family_Income'].map(lmh)
+    df['Teacher_Quality'] = df['Teacher_Quality'].map(lmh)
+    df['School_Type'] = df['School_Type'].map({
+        'Public': +1,
+        'Private': -1,
+    })
+    df['Peer_Influence'] = df['Peer_Influence'].map({
+        'Positive': +1,
+        'Neutral': 0,
+        'Negative': -1,
+    })
+    df['Learning_Disabilities'] = df['Learning_Disabilities'].map(yn)
+    df['Parental_Education_Level'] = df['Parental_Education_Level'].map({
+        'Postgraduate': +3,
+        'College': +2,
+        'High School': +1,
+    }).fillna(0)
+    df['Distance_from_Home'] = df['Distance_from_Home'].map({
+        'Near': +1,
+        'Moderate': 0,
+        'Far': -1,
+    }).fillna(0)
+    df['Gender'] = df['Gender'].map({
+        'Female': +1,
+        'Male': -1,
+    }).fillna(0)
+
+    train_df, val_df = train_test_split(
+        df,
+        test_size=TEST_SAMPLE_SIZE,
+        random_state=RANDOM_STATE
+    )
+
+    return (
+        tensor_dataset_from_dataframe(
+            df=train_df,
+            target=target,
+            y_dtype=REGRESSION_TASK.dtype
+        ),
+        tensor_dataset_from_dataframe(
+            df=val_df,
+            target=target,
+            y_dtype=REGRESSION_TASK.dtype
+        )
+    )
+
+## 1.4.8 Dataset factories
+
+def make_dataset1(batch_size: int) -> Dataset:
+    train_dataset, test_dataset = breast_cancer_dataset()
+
+    return Dataset(
+        name='wisc_bc_data.csv',
+        number=1,
+        features_count=train_dataset[1][0].shape[0],
+        classes_count=2,
+        data=DatasetData(
+            train_dataset=train_dataset,
+            test_dataset=test_dataset,
+            train_loader=DataLoader(
+                train_dataset,
+                batch_size=batch_size,
+                shuffle=True
+            ),
+            test_loader=DataLoader(
+                test_dataset,
+                batch_size=batch_size,
+                shuffle=False
+            )
+        )
+    )
+
+def make_dataset2(batch_size: int) -> Dataset:
+    train_dataset, test_dataset = car_evaluation_dataset()
+
+    return Dataset(
+        name='car_evaluation.csv',
+        number=2,
+        features_count=train_dataset[1][0].shape[0],
+        classes_count=4,
+        data=DatasetData(
+            train_dataset=train_dataset,
+            test_dataset=test_dataset,
+            train_loader=DataLoader(
+                train_dataset,
+                batch_size=batch_size,
+                shuffle=True
+            ),
+            test_loader=DataLoader(
+                test_dataset,
+                batch_size=batch_size,
+                shuffle=False
+            )
+        )
+    )
+
+def make_dataset3(batch_size: int) -> Dataset:
+    train_dataset, test_dataset = student_performance_factors_dataset()
+
+    return Dataset(
+        name='StudentPerformanceFactors.csv',
+        number=3,
+        features_count=train_dataset[1][0].shape[0],
+        classes_count=1,
+        data=DatasetData(
+            train_dataset=train_dataset,
+            test_dataset=test_dataset,
+            train_loader=DataLoader(
+                train_dataset,
+                batch_size=batch_size,
+                shuffle=True
+            ),
+            test_loader=DataLoader(
+                test_dataset,
+                batch_size=batch_size,
+                shuffle=False
+            )
+        )
+    )
+
+DATASETS = [
+    make_dataset1(batch_size=12),
+    make_dataset2(batch_size=12),
+    make_dataset3(batch_size=12),
+]
+
+## 1.4.9 Custom layers
+
+class CustomBackwardFunction(torch.autograd.Function):
+    """
+    Переопределённая функция для линейного слоя.
+    """
+    @staticmethod
+    def forward(
+        ctx,
+        p: float,
+        input: torch.Tensor,
+        weight: torch.Tensor,
+        bias: float = None
+    ):
+        ctx.save_for_backward(torch.scalar_tensor(p), input, weight, bias)
+
+        output = input.mm(weight.t())
+        if bias is not None:
+            output += bias
+
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        p, input, weight, bias = ctx.saved_tensors
+
+        height = weight.size(0)
+        bernoulli_mask = torch.bernoulli(torch.ones(height) * (1 - p.item()))
+ 
+        diagonal_mask = torch.diag(bernoulli_mask) / (1 - p.item())
+
+        grad_output = grad_output.mm(diagonal_mask)
+
+        grad_input = grad_output.mm(weight)
+        grad_weight = grad_output.t().mm(input)
+
+        if bias is not None:
+            grad_bias = grad_output.sum(0)
+        else:
+            grad_bias = None
+
+        return None, grad_input, grad_weight, grad_bias
+
+class CustomReLUBackwardFunction(torch.autograd.Function):
+    """
+    Переопределённая функция для слоя ReLU.
+    """
+    @staticmethod
+    def forward(ctx, p: float, input: torch.Tensor):
+        ctx.save_for_backward(torch.scalar_tensor(p), input)
+        return F.relu(input)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        p, input = ctx.saved_tensors
+
+        grad_output = grad_output * (input > 0).float()
+
+        # У матриц ось 0 это Y
+        height = grad_output.size(0)
+        bernoulli_mask = torch.bernoulli(torch.ones(height) * (1 - p.item()))
+        diagonal_mask = torch.diag(bernoulli_mask) / (1 - p.item())
+
+        diagonal_mask = diagonal_mask.unsqueeze(1).expand(-1, grad_output.size(1), -1)
+        diagonal_mask = diagonal_mask.permute(0, 2, 1)
+
+        grad_output = grad_output.unsqueeze(1) * diagonal_mask
+        grad_output = grad_output.sum(dim=1)
+
+        return None, grad_output
+
+    
+class CustomReLUFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, p: float):
+        ctx.save_for_backward(input, torch.scalar_tensor(p))
+        return input.clamp(min=0)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, p, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input <= 0] = 0
+
+        # У матриц ось 0 это Y (Добавляем аргумент device=grad_output.device для указания устройства для создания тензора grad_input)
+        # XXX 2. grad_input.size(0) на grad_input.size(1)
+        bernoulli_mask = torch.bernoulli(torch.ones(grad_input.size(0), device=grad_output.device) * (1 - p.item()))
+        # XXX 1. Попробовать запустить без деления
+        diagonal_mask = torch.diag(bernoulli_mask) # / (1 - p.item()+1e-5)
+
+        # Перемещаем diagonal_mask на Cuda
+        diagonal_mask = diagonal_mask.to(grad_output.device)
+        
+        # Multiply grad_input with the diagonal matrix
+        # XXX 2. Заменить на grad_input @ diagonal_mask
+        grad_input = diagonal_mask @ grad_input
+        
+        return grad_input, None
+    
+    
+class CustomReLULayer(torch.nn.Module):
+    def __init__(self, p: float):
+        super(CustomReLULayer, self).__init__()
+        self.p = p
+        self.custom_relu_backward = CustomReLUFunction.apply
+
+    def forward(self, x):
+        return self.custom_relu_backward(x, self.p)
+
+
+## 1.4.10 Neural networks
+
+class RegularNetwork(nn.Module):
+    """
+    Нейросеть с обычными линейными слоями. Параметр `p` игнорируется.
+    """
+    def __init__(self, inputs_count: int, outputs_count: int, p: float):
+        super(RegularNetwork, self).__init__()
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(inputs_count, 32 * 32)
+        self.fc2 = nn.Linear(32 * 32, 32 * 32)
+        self.fc3 = nn.Linear(32 * 32, outputs_count)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
+
+
+class AugmentedLinearNetwork(nn.Module):
+    """
+    Нейросеть с переопределённой функцией распространения ошибки
+    для линейных слоёв.
+    """
+    def __init__(self, inputs_count: int, outputs_count: int, p: float):
+        super(AugmentedReLUNetwork, self).__init__()
+
+        self.flatten = nn.Flatten()
+        self.p = p
+
+        self.fc1 = nn.Linear(inputs_count, 32 * 32)
+        self.fc2 = nn.Linear(32 * 32, 32 * 32)
+        self.fc3 = nn.Linear(32 * 32, outputs_count)
+
+        self.custom_backward = CustomBackwardFunction.apply
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.custom_backward(self.p, x, self.fc1.weight, self.fc1.bias)
+        x = F.relu(x)
+        x = self.custom_backward(self.p, x, self.fc2.weight, self.fc2.bias)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
+    
+# XXX 3. Расширить внутренний слой??? 
+
+class AugmentedReLUNetwork(nn.Module):
+    """
+    Нейросеть с переопределённой функцией распространения ошибки
+    для функции активации.
+    """
+    def __init__(self, inputs_count: int, outputs_count: int, p: float):
+        super(AugmentedReLUNetwork, self).__init__()
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(inputs_count, 32 * 32)
+        self.fc2 = nn.Linear(32 * 32, 32 * 32)
+        self.fc3 = nn.Linear(32 * 32, outputs_count)
+
+        self.custom_relu1 = CustomReLULayer(p)
+        self.custom_relu2 = CustomReLULayer(p)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.custom_relu1(x)
+        x = self.fc2(x)
+        x = self.custom_relu2(x)
+        x = self.fc3(x)
+        return x
+

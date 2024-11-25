abda053c5e319cfd607e2af98e4854d61b4299d8
Maria Zaitseva <maria@tail-call.ru>
2024-11-25 21:48:44 +0700

    Ноука

--- cgtnnlib/CustomReLUFunction.py
+++ cgtnnlib/CustomReLUFunction.py
@@ -8,6 +8,10 @@ class MockCtx:
     def save_for_backward(self, input, tensor):
         self.input = input
         self.tensor = tensor
+        
+def pprint(*args):
+    # print(*args)
+    return
 
 class CustomReLUFunction(torch.autograd.Function):
     @staticmethod
@@ -20,13 +24,25 @@ class CustomReLUFunction(torch.autograd.Function):
     def backward(ctx, grad_output):
         input, p = ctx.saved_tensors
         grad_input = grad_output.clone()
+    
         grad_input[input <= 0] = 0
+        pprint(">>> grad_input[input <= 0] = 0")
+        pprint(">>> grad_input")
+        pprint(grad_input)
 
         # У матриц ось 0 это Y (Добавляем аргумент device=grad_output.device для указания устройства для создания тензора grad_input)
         # XXX 2. grad_input.size(0) на grad_input.size(1)
         bernoulli_mask = torch.bernoulli(torch.ones(grad_input.size(0), device=grad_output.device) * (1 - p.item()))
-        # YYY 1. Попробовать запустить без деления
-        diagonal_mask = torch.diag(bernoulli_mask) # / (1 - p.item()+1e-5)
+        pprint(">>> bernoulli_mask = torch.bernoulli(torch.ones(grad_input.size(0), device=grad_output.device) * (1 - p.item()))")
+        pprint(">>> bernoulli_mask")
+        pprint(bernoulli_mask)
+
+        # ??? Непонятно, влияет ли деление это на что-либо
+        # diagonal_mask = torch.diag(bernoulli_mask) / (1 - p.item()+1e-5)
+        diagonal_mask = torch.diag(bernoulli_mask)
+        pprint(">>> diagonal_mask = torch.diag(bernoulli_mask)")
+        pprint(">>> diagonal_mask")
+        pprint(diagonal_mask)
 
         # Перемещаем diagonal_mask на Cuda
         diagonal_mask = diagonal_mask.to(grad_output.device)
@@ -34,6 +50,13 @@ class CustomReLUFunction(torch.autograd.Function):
         # Multiply grad_input with the diagonal matrix
         # XXX 2. Заменить на grad_input @ diagonal_mask
         grad_input = diagonal_mask @ grad_input
+        pprint(">>> grad_input = diagonal_mask @ grad_input")
+        pprint(">>> grad_input")
+        pprint(grad_input)
+
+        pprint("ON THE OTHER HAND:")
+        pprint(">>> grad_input @ diagonal_mask ")
+        pprint(grad_input @ diagonal_mask)
 
         return grad_input, None
     
--- cgtnnlib/common.py
+++ cgtnnlib/common.py
@@ -22,10 +22,14 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
+import torch.optim as optim
 
 import matplotlib.pyplot as plt
 
+from cgtnnlib.AugmentedReLUNetwork import AugmentedReLUNetwork
 from cgtnnlib.RegularNetwork import RegularNetwork
+from cgtnnlib.TrainingParameters import TrainingParameters
+from cgtnnlib.LearningTask import classification_task, regression_task
 
 from .Dataset import Dataset
 from .ExperimentParameters import ExperimentParameters
@@ -372,3 +376,90 @@ if __name__ == "__main__":
     print('Datasets:')
     for dataset in DATASETS:
         print(f"{dataset.number}) {dataset.name}: {dataset.features_count} features, {dataset.classes_count} classes")
+
+def train_main():
+    for experiment_params in iterate_experiment_parameters():
+        fig, axs = plt.subplots(2, 3, sharey='col', figsize=(10, 12))
+        fig.set_size_inches(35, 20)
+
+        for training_params in [
+            TrainingParameters(
+                dataset=DATASETS[0],
+                criterion=classification_task.criterion,
+                experiment_params=experiment_params,
+                model_a_path=DATASETS[0].model_a_path(experiment_params),
+                model_b_path=DATASETS[0].model_b_path(experiment_params),
+                loss_curve_plot_col_index=0,
+            ),
+            TrainingParameters(
+                dataset=DATASETS[1],
+                criterion=classification_task.criterion,
+                experiment_params=experiment_params,
+                model_a_path=DATASETS[1].model_a_path(experiment_params),
+                model_b_path=DATASETS[1].model_b_path(experiment_params),
+                loss_curve_plot_col_index=1,
+            ),
+            TrainingParameters(
+                dataset=DATASETS[2],
+                criterion=regression_task.criterion,
+                experiment_params=experiment_params, 
+                model_a_path=DATASETS[2].model_a_path(experiment_params),
+                model_b_path=DATASETS[2].model_b_path(experiment_params),
+                loss_curve_plot_col_index=2,
+            )
+        ]:
+            inputs_count = training_params.dataset.features_count
+            outputs_count = training_params.dataset.classes_count
+
+
+            for (model, name, row) in [
+                (RegularNetwork(
+                    inputs_count=inputs_count,
+                    outputs_count=outputs_count,
+                    p=experiment_params.p
+                ), training_params.model_a_path, 0),
+                (AugmentedReLUNetwork(
+                    inputs_count=inputs_count,
+                    outputs_count=outputs_count,
+                    p=experiment_params.p
+                ), training_params.model_b_path, 1)
+            ]:
+                model.apply(init_weights)
+
+                model = model.to(device)
+
+                running_losses = train(
+                    model=model,
+                    dataset=training_params.dataset,
+                    epochs=EPOCHS,
+                    experiment_parameters=experiment_params,
+                    criterion=training_params.criterion,
+                    optimizer=optim.Adam(
+                        model.parameters(),
+                        lr=LEARNING_RATE
+                    )
+                )
+
+                torch.save(model.state_dict(), name)
+
+                report_key = f'loss_{type(model).__name__}_{training_params.dataset.number}_p{experiment_params.p}_N{experiment_params.iteration}'
+
+                append_to_report(report_key, running_losses)
+
+                col = training_params.loss_curve_plot_col_index
+
+                plot_loss_curve(
+                    ax=axs[row, col],
+                    model_name=model.__class__.__name__,
+                    dataset_name=training_params.dataset.name,
+                    dataset_number=training_params.dataset.number,
+                    running_losses=running_losses,
+                    p=experiment_params.p,
+                    iteration=experiment_params.iteration
+                )
+
+        path = save_plot(f'loss__p{experiment_params.p}_N{experiment_params.iteration}')
+        print('train_main(): path = ', path)
+
+if __name__ == '__main__':
+    train_main()
